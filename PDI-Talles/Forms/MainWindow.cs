ee336697bd889b201da1d30782fdaaee320d4e8e
Talles <tallesludwig@gmail.com>
2017-08-19 19:52:25 -0300

    Quadrantes

--- PDI-Talles/Forms/MainWindow.cs
+++ PDI-Talles/Forms/MainWindow.cs
@@ -20,20 +20,34 @@ namespace PDI_Talles.Forms
             InitializeComponent();
         }
 
-        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        private void UploadPiture(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 this.img.Image = _fileController.LoadImg(ofd.FileName);
-                ////funDeCinzaToolStripMenuItem.Visible = true;
-                ////imagemOriginalToolStripMenuItem.Visible = true;
+                originalImage.Visible = true;
             }
+        }
+
+        private void GetOriginalImage(object sender, EventArgs e)
+        {
+            this.img.Image = _fileController.GetOriginalImage();
+            this.img1.Image = this.img2.Image = this.img3.Image = this.img4.Image = null;
+            this.img.Visible = true;
+            this.img1.Visible = this.img2.Visible = this.img3.Visible = this.img4.Visible = false;
+        }
+
+        private void Quadrants(object sender, EventArgs e)
+        {
+            this.img.Visible = false;
+            this.img1.Visible = this.img2.Visible = this.img3.Visible = this.img4.Visible = true;
+
+            var slicedImg = _fileController.SliceImg(4);
+            this.img1.Image = slicedImg[0];
+            this.img2.Image = slicedImg[1];
+            this.img3.Image = slicedImg[2];
+            this.img4.Image = slicedImg[3];
         }
     }
 }
